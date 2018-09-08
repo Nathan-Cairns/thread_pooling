1a3d35bc19c88e94dea7d98d4a5263d1bedd5742
Nathan Cairns <nathancairns777@gmail.com>
2018-09-08 12:47:24 +1200

    Write num_cores script

--- src/num_cores.c
+++ src/num_cores.c
@@ -1,1 +1,10 @@
 #include <stdio.h>
+#include <sys/sysinfo.h>
+
+/* This program gets the number of cores on the current machine and prints the result to the
+ * command line */
+int main() {
+    int number_of_cores = get_nprocs_conf();
+    printf("This machine has %d cores\n", number_of_cores);
+    return 0;
+}
